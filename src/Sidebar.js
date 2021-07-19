ba9bde43031e6f43c2ec3fb7e580a90263e1a33a
Calvin Putera Loka <calvinputeraloka@gmail.com>
2021-07-19 16:32:16 +0700

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twitter Icon */}
+
+            {/* Sidebar Option  */}
+        </div>
+    )
+}
+
+export default Sidebar
